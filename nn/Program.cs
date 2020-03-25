393a26c9cd89b57cb5a9d3a67f61875ff646a7ee
fzhonin <artrelax22@gmail.com>
2020-03-25 19:32:44 +0200

    Commentaries added

--- nn/Program.cs
+++ nn/Program.cs
@@ -2,10 +2,14 @@
 
 namespace Task1
 {
-    
 
     static class NewYear
     {
+        /*
+         * Function returns max number of gifts
+         * Counts the limits of a number of candies/tangerines/apples that can fit the bag
+         * Than brute force all the variations of bag filling from 0->limits
+         */
         static uint CountMaxGiftsNumber(uint candyWeight, uint tangerineWeight, uint appleWeight, uint bagCapacity)
         {
             uint maxNumberOfCandies = bagCapacity / candyWeight;
@@ -27,6 +31,10 @@ namespace Task1
 
             return maxNumberOfGifts;
         }
+        /* 
+        * Gets a value from console, if it's convertable to unsigned int and the value is not a 0, 
+        * returns an unsigned int value, otherwise writes a message asking to enter a new value while not correct.
+        */
         private static uint GetUIntFromConsole()
         {
             uint value = 0;
@@ -45,8 +53,7 @@ namespace Task1
 
         static void Main(string[] args)
         {
-            while (true)
-            {
+            
                 Console.WriteLine("Enter the weight of a candy");
                 uint x1 = GetUIntFromConsole();
                 Console.WriteLine("Enter the weight of a tangerine");
@@ -56,8 +63,8 @@ namespace Task1
                 Console.WriteLine("Enter bag capacity");
                 uint w1 = GetUIntFromConsole();
                 Console.Write("Max number of gifts is: ");
-                Console.WriteLine(CountMaxGiftsNumber(x1, y1, z1, w1));
-            }
+                Console.WriteLine(CountMaxGiftsNumber(x1, y1, z1, w1)); //Function invokation
+            
         }
     }
 }
